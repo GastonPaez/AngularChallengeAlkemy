894d364d9a0a0cd1f593488785a718f9429b4f1d
Gaston <paez.gastonm@gmail.com>
2022-09-20 23:09:25 -0300

    navarbar

---
+++ restaurant/src/app/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: [
+    './navbar.component.css',
+    './style.css',
+    './responsive.css',
+]
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
